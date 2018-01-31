1352482d2f62e662cbebb45513aec056fccae562
Ion Agorria <ion@agorria.com>
2018-01-31 19:46:35 +0100

    First commit

---
+++ convert.php
@@ -0,0 +1,203 @@
+<?php
+/*
+ * Copyright (C) 2014      Ion Agorria          <ion@agorria.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  \file       convert.php
+ *  \ingroup    proposaltoorder
+ *  \brief      ProposalToOrder conversion code
+ */
+
+//TODO: Copy dates
+
+//include depending of root or custom directory
+$result=@include("../main.inc.php");
+if (! $result) {
+    $result=@include("../../main.inc.php");
+}
+
+require_once DOL_DOCUMENT_ROOT.'/product/class/product.class.php';
+require_once DOL_DOCUMENT_ROOT.'/comm/propal/class/propal.class.php';
+require_once DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.commande.class.php';
+require_once DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.product.class.php';
+
+if (! empty($conf->projet->enabled))
+{
+    require_once DOL_DOCUMENT_ROOT.'/projet/class/project.class.php';
+}
+
+function convert_propal($db, $propal_id) {
+    global $user;
+    //Load the propal object instance and fetch the data from DB
+    $propal = new Propal($db);
+    $ret = $propal->fetch($propal_id);
+    if ($ret < 0) dol_print_error('',$propal->error);
+    if ($ret > 0) {
+        $created_orders = array(); //List of suppliers that have a order associated, format (fourn_socid => order_id)
+        //Iterate each line in propal
+        foreach($propal->lines as $array_line)
+        {
+            $fk_product = $array_line->fk_product;
+            if (!empty($fk_product)) {
+                $product = new Product($db);
+                $product->fetch($fk_product);
+                //product from line has ID, lets get supplier
+                $product_fourn = new ProductFournisseur($db);
+                $ret = $product_fourn->find_min_price_product_fournisseur($product->id);
+                if ($ret > 0) {
+                    //We found the supplier for this product, lets check if we have a already existing
+                    //order for this supplier, if not, create a new one
+                    $fourn_socid = $product_fourn->fourn_id;
+                    //Create a supplier order object instance
+                    $order_supplier = new CommandeFournisseur($db);
+                    if (array_key_exists($fourn_socid, $created_orders)) {
+                        //Get the existing order
+                        $order_id = $created_orders[$fourn_socid];
+                        $order_supplier->fetch($order_id);
+                    } else {
+                        //Put the important values to the empty order
+                        $order_supplier->socid = $fourn_socid;
+                        //Create to db
+                        $order_supplier->create($user);
+                        //Get the id
+                        $order_id = $order_supplier->id;
+                        //Assign the project which is linked this order
+                        $order_supplier->setProject($propal->fk_project, $order_id);
+                        $created_orders[$fourn_socid] = $order_id;
+                    }
+                    order_supplier_addline($db, $order_supplier, $product, $array_line);
+                }
+            }
+        }
+        
+        $extrafields = new ExtraFields($db);
+        $extrafields->fetch_name_optionals_label('commande_fournisseur'); //Get extrafields of order
+        foreach($created_orders as $fourn_socid => $order_id){
+            $order_supplier = new CommandeFournisseur($db);
+            $order_supplier->fetch($order_id);
+            $order_supplier->update_price(); //Update the prices and stuff...
+
+            foreach($extrafields->attribute_label as $key=>$label)
+            {
+                 //Check if order extrafield has same value for propal and store in order if so
+                $value=$propal->array_options["options_".$key];
+                if (!empty($value)) {
+                    $order_supplier->array_options["options_".$key] = $value;
+                }
+            }
+            //Write array_options to db
+            $order_supplier->insertExtraFields();
+        }
+    }
+}
+
+function order_supplier_addline($db, $order_supplier, $product, $propal_line)
+{
+    global $langs;
+
+    //Get the data and put into variables
+    $fk_commande        = $order_supplier->id;
+    $fk_product         = $propal_line->fk_product;
+    $label              = $product->label; //Product object has more details
+    $desc               = $product->description;  //Product object has more details
+    $product_type       = $propal_line->product_type;
+    $qty                = $propal_line->qty;
+    $tva_tx             = $propal_line->tva_tx;
+    $remise_percent     = $propal_line->remise_percent;
+    $subprice           = $propal_line->subprice;
+    $ref                = $propal_line->product_ref;
+    $total_ht           = $propal_line->total_ht;
+    $total_tva          = $propal_line->total_tva;
+    $total_ttc          = $propal_line->total_ttc;
+
+    //These are special db values that are not "normally" accesible from class interface, so we use raw sql access
+    $sql = 'SELECT pd.rowid,';
+    $sql.= ' pd.localtax1_tx, pd.localtax1_type, pd.total_localtax1,';
+    $sql.= ' pd.localtax2_tx, pd.localtax2_type, pd.total_localtax2';
+    $sql.= ' FROM '.MAIN_DB_PREFIX.'propaldet as pd';
+    $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'product as p ON pd.fk_product = p.rowid';
+    $sql.= ' WHERE pd.rowid = '.$propal_line->rowid;
+
+    $result = $db->query($sql);
+    if ($result)
+    {
+        $objp = $db->fetch_object($result);
+        $txlocaltax1        = $objp->localtax1_tx;
+        $txlocaltax2        = $objp->localtax2_tx;
+        $localtax1_type     = $objp->localtax1_type;
+        $localtax2_type     = $objp->localtax2_type;
+        $total_localtax1    = $objp->total_localtax1;
+        $total_localtax2    = $objp->total_localtax2;
+        $db->free($result);
+    }
+
+    //Insert the values
+    $sql = "INSERT INTO ".MAIN_DB_PREFIX."commande_fournisseurdet";
+    $sql.= " (fk_commande, label, description,";
+    $sql.= " fk_product, product_type,";
+    $sql.= " qty, tva_tx, localtax1_tx, localtax2_tx, remise_percent, subprice, ref,";
+    $sql.= " total_ht, total_tva, total_localtax1, total_localtax2, total_ttc,";
+    $sql.= " localtax1_type, localtax2_type, date_start, date_end";
+    $sql.= ") VALUES (".$fk_commande.", '" . $db->escape($label) . "','" . $db->escape($desc) . "',";
+    $sql.= (! empty($fk_product)?$fk_product:"null").',';
+    $sql.= "'".$product_type."',";
+    $sql.= "'".$qty."', ".$tva_tx.", ".$txlocaltax1.", ".$txlocaltax2.", ".$remise_percent.",'".$subprice."','".$db->escape($ref)."',";
+    $sql.= "'".$total_ht."',";
+    $sql.= "'".$total_tva."',";
+    $sql.= "'".$total_localtax1."',";
+    $sql.= "'".$total_localtax2."',";
+    $sql.= "'".$total_ttc."',";
+    $sql.= "'".$localtax1_type."',";
+    $sql.= "'".$localtax2_type."',";
+    $sql.= (! empty($propal_line->date_start)?"'".$db->idate($propal_line->date_start)."'":"null").',';
+    $sql.= (! empty($propal_line->date_end)?"'".$db->idate($propal_line->date_end)."'":"null");
+    $sql.= ")";
+
+    dol_syslog("order_supplier_addline sql=".$sql);
+    $resql=$db->query($sql);
+
+    if ($resql)
+    {
+        $rowid = $db->last_insert_id(MAIN_DB_PREFIX.'commande_fournisseurdet');
+        $order_supplier->rowid = $rowid;
+        // Trigger disabled because POST data is incomplete compared to original trigger and causes unpredictable behavior
+        $db->commit();
+        return 1;
+    }
+    else
+    {
+        $error=$db->error();
+        $db->rollback();
+        dol_syslog("order_supplier_addline ".$error, LOG_ERR);
+        return -1;
+    }
+}
+
+//Load from GET/POST
+$mesg = ''; //Disable msg error when nothing happened
+$id = GETPOST('id','int');
+$action = GETPOST('action');
+$confirm = GETPOST('confirm');
+$return = GETPOST('return');
+if ($id > 0 && $confirm == "yes" && $action == "convert" && !empty($return)) {
+    convert_propal($db, $id);
+}
+$db->close();
+if (!empty($return)) {
+    //Send the browser back where it came from
+    print  '<script type="text/javascript">window.location = "'.$return.'?id='.$id.'";</script>';
+}
---
+++ core/modules/modProposalToOrder.class.php
@@ -0,0 +1,143 @@
+<?php
+/*
+ * Copyright (C) 2014      Ion Agorria          <ion@agorria.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 	\defgroup	proposaltoorder	modProposalToOrder module
+ * 	\file		core/modules/modProposalToOrder.class.php
+ * 	\ingroup	proposaltoorder
+ * 	\brief		Converts project proposals to project orders
+ */
+include_once DOL_DOCUMENT_ROOT . "/core/modules/DolibarrModules.class.php";
+
+/**
+ * Description and activation class for module ProposalToOrder
+ */
+class modProposalToOrder extends DolibarrModules
+{
+
+    /**
+    *         Constructor. Define names, constants, directories, boxes, permissions
+    *
+    *         @param        DoliDB                $db        Database handler
+    */
+    public function __construct($db)
+    {
+        global $langs, $conf;
+
+        $this->db = $db;
+
+        // Id for modul.
+        $this->numero = 674501;
+        // Key text used to identify module (for permissions, menus, etc...)
+        $this->rights_class = 'proposaltoorder';
+
+        // Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
+        $this->family = "projects";
+        // Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
+        $this->name = preg_replace('/^mod/i','',get_class($this));
+        // Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
+        $this->description = "Converts Proposal to Order in a project";
+        // Possible values for version are: 'development', 'experimental', 'dolibarr' or version
+        $this->version = '3.7.0-r0.2';
+        // Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
+        $this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
+        // Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
+        $this->special = 2;
+        // Name of image file used for this module.
+        // If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
+        // If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
+        $this->picto='propal';
+
+        //Triggers/Hooks
+        $this->module_parts = array(
+            'hooks' => array('propalcard'), // Set here all hooks context you want to support
+        );
+
+
+        // Data directories to create when module is enabled.
+        $this->dirs = array();
+        $r=0;
+
+        // Config pages. Put here list of php page names stored in admmin directory used to setup module.
+        //$this->config_page_url = array("proposaltoorder.php@proposaltoorder");
+
+        // Dependencies
+        $this->depends = array();       // List of modules id that must be enabled if this module is enabled
+        $this->requiredby = array();                // List of modules id to disable if this one is disabled
+        $this->phpmin = array(5,0);                 // Minimum version of PHP required by module
+        $this->need_dolibarr_version = array(3,2);  // Minimum version of Dolibarr required by module
+        $this->langfiles = array("proposaltoorder@proposaltoorder");
+
+        // Constants
+        $this->const = array();         // List of parameters
+        // Array to add new pages in new tabs
+        $this->tabs = array();
+
+        // Boxes
+        // Add here list of php file(s) stored in includes/boxes that contains class to show a box.
+        $this->boxes = array();         // List of boxes
+        $r=0;
+
+        // Permissions
+        $this->rights_class = 'proposaltoorder';    // Permission key
+        $this->rights = array();            // Permission array used by this module
+    }
+
+    /**
+     *      Function called when module is enabled.
+     *      The init function add constants, boxes, permissions and menus (defined in constructor) into Dolibarr database.
+     *      It also creates data directories.
+     *      @return     int             1 if OK, 0 if KO
+     */
+    function init($options = '')
+    {
+        $sql = array();
+
+        $result=$this->load_tables();
+
+        return $this->_init($sql);
+    }
+
+    /**
+     *      Function called when module is disabled.
+     *      Remove from database constants, boxes and permissions from Dolibarr database.
+     *      Data directories are not deleted.
+     *      @return     int             1 if OK, 0 if KO
+     */
+    function remove($options = '')
+    {
+        $sql = array();
+
+        return $this->_remove($sql);
+    }
+
+
+    /**
+     *      Create tables, keys and data required by module
+     *      Files llx_table1.sql, llx_table1.key.sql llx_data.sql with create table, create keys
+     *      and create data commands must be stored in directory /mymodule/sql/
+     *      This function is called by this->init
+     *
+     *      @return     int     <=0 if KO, >0 if OK
+     */
+    function load_tables()
+    {
+        return 1;
+    }
+}
+?>
